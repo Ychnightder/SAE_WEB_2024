c7af9a1d043003214f990ee751b57d8d64e2a6b0
BaZz Ychnightder <pierreychnightder561@gmail.com>
2024-10-21 18:06:29 +0200

    fini AC

---
+++ shared-js/dataAC.js
@@ -0,0 +1,29 @@
+export const Pays = [
+  "France",
+  "Allemagne",
+  "Italie",
+  "Espagne",
+  "Royaume-Uni",
+  "États-Unis",
+  "Canada",
+  "Japon",
+  "Chine",
+  "Brésil",
+  "Argentine",
+  "Russie",
+  "Inde",
+  "Australie",
+  "Mexique",
+  "Afrique du Sud",
+  "Égypte",
+  "Nigéria",
+  "Corée du Sud",
+  "Turquie",
+];
+export const libelleTel = [
+  "Fax",
+  " Bureau fixe ",
+
+  "Mobile",
+  " Personnel fixe ",
+];
