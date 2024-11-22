627354af64ded45a1b933e2c454d4226d6b26f23
ilian.desbois <ilian.desbois@etu.u-paris.fr>
2024-11-22 10:57:51 +0100

    Commit

--- src/assets/php/adhesion.php
+++ src/assets/php/adhesion.php
@@ -13,6 +13,28 @@ if (!empty($identifiant) && !empty($password)) {
 } else {
     echo "<script>alert('Mets un ID');</script>";
 }
+
+
+$host = '172.16.8.65';
+$db = 'grp208_3';
+$user = 'ilian.desbois';
+$pass = 'c2a44c3f';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connexion réussie !";
+} catch (\PDOException $e) {
+    echo "galère un peu";
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 echo "<script>window.location.href = '../../adhesion-connexion.html';</script>";
 exit();
 ?>
