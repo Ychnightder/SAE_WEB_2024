eb54ee0301d460d98fa5bb32d79fc56ce4c5741d
BaZz Ychnightder <pierreychnightder561@gmail.com>
2024-12-02 00:49:48 +0100

    debut enquete

--- public/main.php
+++ public/main.php
@@ -1,8 +1,31 @@
 
-<?php require_once '../src/php/views/header.php'; ?>
-
-<div class="container">
-    <h1>Bienvenue sur notre enquête</h1>
-    <p>Aidez-nous à améliorer l'accompagnement des personnes autistes.</p>
-    <a href="enquete.php?step=1" class="btn">Commencer l'enquête</a>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/enquete-main.css">
+</head>
+<body>
+<div class="wave fond-top">
+    <img src="./assets/image/enquete/VectorTop.png" alt="une vague">
 </div>
+<a class="retour" href="index.php">Retour l'accueil</a>
+<div class="container">
+    <div class="logo-box">
+        <img src="./assets/image/shared/logo.png" alt="">
+    </div>
+    <div class="body-container">
+        <h1>Bienvenue sur notre enquête</h1>
+        <h1>Aidez nous à améliorer l'accompagnement des personnes autistes et de leurs proches</h1>
+        <a href="enquete.php?step=1" class="btn">Commencer l'enquête</a>
+    </div>
+</div>
+<div class="wave fond-bot">
+    <img src="./assets/image/enquete/Vector.png" alt="une vague">
+</div>
+</body>
+</html>
