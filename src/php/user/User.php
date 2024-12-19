5a9ce0b7ccb790ab3a082356c5cc0326a7ed11a9
Ilian5 <ilian.desbois@etu.u-paris.fr>
2024-12-19 17:47:27 +0100

    User

--- src/php/user/User.php
+++ src/php/user/User.php
@@ -2,26 +2,19 @@
 
 namespace user;
 
+
 class User {
 
     private int $id;
     private string $nom;
     private string $prenom;
     private string $email;
-    private string $password;
-    private string $adresse;
-    private string $telephone;
-    private Date $dateCreation;
 
-    public function __construct($id, $nom, $prenom, $email, $password, $adresse, $telephone, $dateCreation) {
+    public function __construct($id, $nom, $prenom, $email) {
         $this->id = $id;
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->email = $email;
-        $this->password = $password;
-        $this->adresse = $adresse;
-        $this->telephone = $telephone;
-        $this->dateCreation = $dateCreation;
     }
 
     public function getId() {
@@ -36,17 +29,4 @@ class User {
     public function getEmail() {
         return $this->email;
     }
-    public function getPassword() {
-        return $this->password;
-    }
-    public function getAdresse() {
-        return $this->adresse;
-    }
-    public function getTelephone() {
-        return $this->telephone;
-    }
-    public function getDateCreation() {
-        return $this->dateCreation;
-    }
-
 }
