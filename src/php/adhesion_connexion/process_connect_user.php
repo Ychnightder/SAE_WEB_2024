e3fa0cde4ad23cd95d6bb944d8793957e3d36543
BaZz Ychnightder <pierreychnightder561@gmail.com>
2024-12-22 18:06:35 +0100

    ajoute de composer et les villes dans la base

--- src/php/adhesion_connexion/process_connect_user.php
+++ src/php/adhesion_connexion/process_connect_user.php
@@ -1,13 +1,18 @@
 <?php
-require_once __DIR__ . '/../config/database.php'; // Chemin vers database.php
+
+use adhesion_connexion\UserManager;
+
+require_once __DIR__ . "/UserManager.php";
 session_start();
+
 $email = isset($_POST['identifiant']) ? trim($_POST['identifiant']) : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
 
 $errors = [];
+
 $oldInputs = ['identifiant' => $email];
 
-// Vérification des champs vides
+
 if (empty($email)) {
     $errors['identifiant'] = "L'email est obligatoire.";
 }
@@ -15,32 +20,24 @@ if (empty($password)) {
     $errors['password'] = "Le mot de passe est obligatoire.";
 }
 
+
 if (!empty($errors)) {
     $_SESSION['login_errors'] = $errors;
     $_SESSION['old_inputs'] = $oldInputs;
-    header("Location: /connexion.php");
+    header("Location: /login.php");
     exit;
 }
 
-$sql = "SELECT * FROM utilisateurs WHERE email = :email LIMIT 1";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':email', $email, PDO::PARAM_STR);
 
-$stmt->execute();
+$userManager = new UserManager();
+$user = $userManager->authenticate($email, $password);
 
-
-$user = $stmt->fetch();
-
-if ($user && password_verify($password, $user['mot_de_passe']) ) {
+if ($user) {
+    // Connexion réussie
     $_SESSION['user_name'] = $user['nom'];
     $_SESSION['user_prenom'] = $user['prenom'];
     $_SESSION['user_email'] = $user['email'];
-    $sqlInsertOnConnexion = "INSERT INTO connexions (date_connexion_, statut_connexion, id_utilisateur) 
-                                 VALUES (NOW(), 1, :id_utilisateur)";
-    $stmt = $pdo->prepare($sqlInsertOnConnexion);
-    $stmt->bindParam(':id_utilisateur', $user['id_utilisateur'], PDO::PARAM_INT);
-    $stmt->execute();
-
+    $userManager->logConnection($user['id_utilisateur']);
     header("Location: /main.php");
     exit;
 } else {
@@ -50,5 +47,4 @@ if ($user && password_verify($password, $user['mot_de_passe']) ) {
     header("Location: /connexion.php");
     exit;
 }
-
 
