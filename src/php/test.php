e65c814c92c90aff0ae02857cdeaf7e763e90d2b
BaZz Ychnightder <pierreychnightder561@gmail.com>
2025-01-05 00:20:24 +0100

    fini reste juste des ajutement

---
+++ src/php/test.php
@@ -0,0 +1,38 @@
+<?php
+require '../../vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+echo $faker->name;       
+echo $faker->address;   
+echo $faker->email;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
